a7e5ba2928b78bfe46d2325f6a02b2fe4005baf5
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-03-15 18:07:07 +0100

    Add functions to change secret content without deleting the secret

--- tests/integration/test_kube_interface.py
+++ tests/integration/test_kube_interface.py
@@ -1,3 +1,4 @@
+import os
 from time import sleep
 
 import pytest
@@ -44,7 +45,7 @@ def test_create_exists_delete_secret(kubeinterface_name, request):
     kubeinterface = request.getfixturevalue(kubeinterface_name)
 
     with umask_named_temporary_file(
-        mode="w", prefix="spark-dynamic-conf-k8s-", suffix=".conf"
+        mode="w", prefix="spark-dynamic-conf-k8s-", suffix=".conf", dir="/home/ubuntu/"
     ) as t:
         property_file.write(t.file)
 
@@ -60,6 +61,74 @@ def test_create_exists_delete_secret(kubeinterface_name, request):
     assert kubeinterface.exists(
         KubernetesResourceType.SECRET_GENERIC, secret_name, namespace
     )
+
+    kubeinterface.delete(KubernetesResourceType.SECRET_GENERIC, secret_name, namespace)
+
+    assert not kubeinterface.exists(
+        KubernetesResourceType.SECRET_GENERIC, secret_name, namespace
+    )
+
+
+@pytest.mark.parametrize(
+    "kubeinterface_name", [("kubeinterface"), ("lightkubeinterface")]
+)
+def test_delete_secret_content(kubeinterface_name, request):
+    secret_name = "my-secret"
+    namespace = "default"
+
+    property_file = PropertyFile({"key": "value"})
+
+    kubeinterface = request.getfixturevalue(kubeinterface_name)
+
+    with umask_named_temporary_file(
+        mode="w",
+        prefix="spark-dynamic-conf-k8s-",
+        suffix=".conf",
+        dir=os.path.expanduser("~"),
+    ) as t:
+        property_file.write(t.file)
+
+        t.flush()
+
+        kubeinterface.create(
+            KubernetesResourceType.SECRET_GENERIC,
+            secret_name,
+            namespace=namespace,
+            **{"from-env-file": str(t.name)},
+        )
+
+    assert kubeinterface.exists(
+        KubernetesResourceType.SECRET_GENERIC, secret_name, namespace
+    )
+
+    kubeinterface.delete_secret_content(secret_name, namespace)
+
+    kubeinterface.exists(KubernetesResourceType.SECRET_GENERIC, secret_name, namespace)
+
+    secret_content = kubeinterface.get_secret(secret_name, namespace)["data"]
+
+    print(f"Secret content: {secret_content}")
+    assert len(secret_content.keys()) == 0
+
+    kubeinterface.add_secret_content(secret_name, namespace, property_file)
+    secret_content = kubeinterface.get_secret(secret_name, namespace)["data"]
+
+    print(f"Secret content: {secret_content}")
+    assert len(secret_content.keys()) == 1
+
+    kubeinterface.add_secret_content(secret_name, namespace, property_file)
+    secret_content = kubeinterface.get_secret(secret_name, namespace)["data"]
+
+    print(f"Secret content: {secret_content}")
+    assert len(secret_content.keys()) == 1
+
+    property_file_1 = PropertyFile({"key": "value", "key1": "value"})
+    kubeinterface.add_secret_content(secret_name, namespace, property_file_1)
+
+    secret_content = kubeinterface.get_secret(secret_name, namespace)["data"]
+
+    print(f"Secret content: {secret_content}")
+    assert len(secret_content.keys()) == 2
 
     kubeinterface.delete(KubernetesResourceType.SECRET_GENERIC, secret_name, namespace)
 
